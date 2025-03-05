94798c5cc6101db2762b080f425f1a07b2c3e5e8
SShreya22 <shreya.s.22052005@gmail.com>
2025-03-05 22:40:50 +0530

    edit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCah4XZjQc7XYoNgs1_LyHIuKN2rd5jMqc",
-    authDomain: "itask-ed4ef.firebaseapp.com",
-    projectId: "itask-ed4ef",
-    storageBucket: "itask-ed4ef.firebasestorage.app",
-    messagingSenderId: "977508587564",
-    appId: "1:977508587564:web:3403d47cf77368b4605989",
-    measurementId: "G-HXW1EFC2HT"
-  };
-  
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
